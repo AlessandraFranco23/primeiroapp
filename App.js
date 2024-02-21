ef2d12baff11f5d50511a769945028527f731f0d
AlessandraFranco23 <alessandrafranco2311@gmail.com>
2024-02-20 21:59:11 -0300

    Inicio- Buscar CEP

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
 
 export default function App() {
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState({
+    logradouro: "",
+    uf: "",
+    localidade: "",
+  });
+  function buscaCep(){
+    console.log(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`, {
+      method: 'GET',
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        setEndereco({
+            logradouro: data.logradouro,
+            uf: data.uf,
+            localidade: data.localidade,
+        }
+        );
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start aworking on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Text style={style.h1}>Busca CEP</Text>
+      <Text style={style.h2}>Encontrar endereço pelo CEP</Text>
+      <TextInput 
+      value={cep}
+      onChangeText={setCep} 
+      maxLength={8} 
+      inputMode="numeric"
+      placeholder='Digite seu CEP'
+      style={style.inputText} 
+      onBlur={buscaCep}
+       />
+       <TextInput
+        style={style.inputText}
+        value={endereco.logradouro}
+        placeholder="Rua / Logradouro"
+
+      />
+      <TextInput
+        style={style.inputText}
+        value={endereco.uf}
+        placeholder="uf"
+
+      />
+      <TextInput
+        style={style.inputText}
+        value={endereco.localidade}
+        placeholder="localidade"
+
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = StyleSheet.create(
+  {
+    h1:{
+      fontSize:20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    h2:{
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    container: {
+      flex: 1,
+      alignContent: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    containerPadding: {
+     padding: 20,
+     alignSelf: 'stretch',
+     backgroundColor: '#fff', 
+    },
+    inputText:{
+      borderWidth: 1,
+      borderColor: "#000",
+      borderRadius: 5,
+      padding: 10,
+      alignSelf: 'stretch',
+    }
+  }
+);
